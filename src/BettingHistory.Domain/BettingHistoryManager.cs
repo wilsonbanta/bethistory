15c0ae1dd52344c36e1298e6435a3821588e2d5d
Wilson Banta <wilson.banta@stickytickets.com.au>
2016-09-08 01:32:01 +0800

    code clean up: remove unused directives

--- src/BettingHistory.Domain/BettingHistoryManager.cs
+++ src/BettingHistory.Domain/BettingHistoryManager.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net.Sockets;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 using BettingHistory.Dto;
 using BettingHistory.Infrastructure;
 using BettingHistory.Model;
