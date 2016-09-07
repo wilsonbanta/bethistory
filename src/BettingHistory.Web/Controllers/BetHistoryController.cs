fbb754cc8ab5f175959c44557bb6d31c5a6612fa
Wilson Banta <wilson.banta@stickytickets.com.au>
2016-09-08 00:15:03 +0800

    prepare page

---
+++ src/BettingHistory.Web/Controllers/BetHistoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BettingHistory.Web.Controllers
+{
+    public class BetHistoryController : Controller
+    {
+        // GET: BetHistory
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
