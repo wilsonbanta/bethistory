f08a8a25e6249eb1b72e017db5897118a8a1d32c
Wilson Banta <wilson.banta@stickytickets.com.au>
2016-09-07 20:54:13 +0800

    initial code commit

---
+++ src/BettingHistory.Model/Settled.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BettingHistory.Model
+{
+	public class Settled
+	{
+		public int Customer { get; set; }
+		public int Event { get; set; }
+		public int Participant { get; set; }
+		public decimal Stake { get; set; }
+		public decimal Win { get; set; }
+	}
+}
