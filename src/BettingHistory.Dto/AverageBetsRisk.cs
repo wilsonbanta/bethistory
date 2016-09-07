19525ac3b180a04e2858aa60e55250c164860755
Wilson Banta <wilson.banta@stickytickets.com.au>
2016-09-07 23:29:44 +0800

    add dto for highwinningstake and averagebetsrisk

---
+++ src/BettingHistory.Dto/AverageBetsRisk.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BettingHistory.Dto
+{
+	public class AverageBetsRisk
+	{
+		public int Customer { get; set; }	
+		public decimal PreviousStakes { get; set; }
+		public decimal CurrentStakes { get; set; }
+		public decimal IncreaseStakes { get; set; }
+	}
+}
---
+++ src/BettingHistory.Dto/HighWinningStake.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BettingHistory.Dto
+{
+	public class HighWinningStake
+	{
+		public int Customer { get; set; }
+		public decimal Stake { get; set; }
+		public decimal ToWin { get; set; }
+	}
+}
